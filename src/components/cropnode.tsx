3b134ce2e606b439ba9a97148280804d741ce70e
zcfabra <christianwgnam@gmail.com>
2023-01-05 12:03:17 -0500

    bounding rects

--- src/components/cropnode.tsx
+++ src/components/cropnode.tsx
@@ -1,6 +1,8 @@
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import CustomNode from "./node"
-import { Node, useReactFlow } from 'reactflow'
+import { Node, rectToBox, useReactFlow } from 'reactflow'
+import { number } from 'zod'
+import { resolveTxt } from 'dns/promises'
 const CropNode:React.FC<{selected:boolean,id:string, data:{
     imgData: Blob | string | null,
     imgRef: React.RefObject<HTMLCanvasElement>,
@@ -9,9 +11,12 @@ const CropNode:React.FC<{selected:boolean,id:string, data:{
 
 }}> = ({selected, id, data}) => {
     const {setNodes} = useReactFlow();
+    const [cropZone, setCropZone] = useState<{x:number, y:number, width: number,height:number, scalingFactor:number} | null>(null)
+    const [localImage, setLocalImage] = useState<ImageBitmap>();
+    const [startingDragPoint, setStartingDragPoint] = useState<{x:number,y:number} | null>(null);
     const localCanvasRef = useRef<HTMLCanvasElement>(null);
     useEffect(()=>{
-        console.log("CROPNODE",data.imgData);
+        // console.log("CROPNODE",data.imgData);
         localCanvasRef.current!.width = data.imgRef.current!.width
         localCanvasRef.current!.height = data.imgRef.current!.height
         const draw = async (blob:Blob)=>{
@@ -19,6 +24,10 @@ const CropNode:React.FC<{selected:boolean,id:string, data:{
             const ctx = localCanvasRef.current?.getContext("2d");
             if (ctx){
                 ctx.drawImage(bitmap, 0,0);
+                setLocalImage(bitmap);
+                if (cropZone !=null){
+
+                }
                 ctx.canvas.toBlob(blob=>{
                     data.setNodes(prev => {
                         const idx = prev.findIndex(x => x.id == id);
@@ -47,15 +56,44 @@ const CropNode:React.FC<{selected:boolean,id:string, data:{
         }
 
         
-    }, [data.imgData]);
+    }, [data.imgData, cropZone]);
 
-    const handleDrawBox= (e:React.DragEvent)=>{
-        e.stopPropagation()
-        console.log(e)
+    const handleDrawBox= (e:React.MouseEvent<HTMLCanvasElement>)=>{
+        if (startingDragPoint != null){
+            const ctx= localCanvasRef.current?.getContext('2d');
+            if (ctx){
+                ctx.clearRect(0,0, localCanvasRef.current!.width, localCanvasRef.current!.height)
+                ctx.drawImage(localImage!, 0, 0)
+
+                const rect = e.currentTarget.getBoundingClientRect();
+                console.log(e.clientX - rect.left - startingDragPoint.x, e.clientY - rect.top - startingDragPoint.y)
+                ctx.beginPath(); 
+                ctx.strokeStyle = "red";
+                const scalingFactor = localCanvasRef.current!.width / rect.width;
+                ctx.rect(startingDragPoint.x * scalingFactor, startingDragPoint.y * scalingFactor, (e.clientX - rect.left - startingDragPoint.x) * scalingFactor, (e.clientY - rect.top  - startingDragPoint.y) * scalingFactor);
+                ctx.stroke()
+                ctx.closePath()
+            }
+
+        }
+    }
+    const handleDragStart = (e: React.MouseEvent<HTMLCanvasElement>)=>{
+        const rect = e.currentTarget.getBoundingClientRect();
+        // const scalingFactor = localCanvasRef.current!.width / rect.width
+        const scalingFactor = 1
+        console.log("YO", rect.left, rect.top, rect.width, rect.height)
+        console.log(e.clientX - rect.left, e.clientY - rect.top)
+        setStartingDragPoint({x: (e.clientX - rect.left) * scalingFactor  , y:(e.clientY -rect.top) *scalingFactor})
+    }
+    const handleDragEnd = (e:React.MouseEvent<HTMLCanvasElement>)=>{
+        const rect = e.currentTarget.getBoundingClientRect()
+        const scalingFactor = localCanvasRef.current!.width/ rect.width;
+        // setCropZone({ x: (e.clientX - rect.left) * scalingFactor, y: (e.clientY - rect.top) * scalingFactor, width: (e.clientX - rect.left - startingDragPoint!.x) * scalingFactor, height:(e.clientY - rect.top - startingDragPoint!.y) * scalingFactor, scalingFactor:scalingFactor})
+        setStartingDragPoint(null);
     }
     return (
-      <CustomNode inputHandle selected={selected} id={id} data={data} overrideStyles={"!w-96 !h-96"}>
-        <canvas onDrag={handleDrawBox} ref={localCanvasRef}></canvas>
+      <CustomNode inputHandle selected={selected} id={id} data={data} overrideStyles={"!w-96 !h-96 !overflow-scroll"}>
+        <canvas className='cursor-crosshair' onMouseLeave={handleDragEnd} onMouseUp={handleDragEnd} onMouseDown={handleDragStart} onMouseMove={handleDrawBox} ref={localCanvasRef}></canvas>
 
         <div className='w-6 h-6 bg-gray-800 rounded-md drag-handle absolute top-4 right-4'></div>
       </CustomNode>

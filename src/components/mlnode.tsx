cb7ef6f60c6a66086374365176dfcf499d8a383e
zcfabra <christianwgnam@gmail.com>
2023-01-08 22:25:09 -0500

    zoom+enhance

--- src/components/mlnode.tsx
+++ src/components/mlnode.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import CustomNode, { NodeData } from "./node"
 import * as tf from "@tensorflow/tfjs"
 import { applyConnectedNodes, clearConnectedNodes } from '../utils/connected';
@@ -7,6 +7,7 @@ const modelURL = "http://localhost:3000/esr/model.json"
 
 const MLNode:React.FC<{data: NodeData, id:string, selected: boolean}> = ({ data, id, selected}) => {
     const [model, setModel] = useState<tf.GraphModel | tf.LayersModel>();
+    const localHiddenCanvasRef = useRef<HTMLCanvasElement>(null);
     useEffect(()=>{
         (async ()=>{
             const model = await tf.loadGraphModel(modelURL, { fromTFHub: false, requestInit: {
@@ -34,12 +35,29 @@ const MLNode:React.FC<{data: NodeData, id:string, selected: boolean}> = ({ data,
     const handleApplyModel = async ()=>{
         if (data.imgData !=null){
             const bitmap = await createImageBitmap(data.imgData);
-            let input = tf.browser.fromPixels(bitmap).div(tf.scalar(255)).toFloat();
+            // let input = tf.browser.fromPixels(bitmap).div(tf.scalar(255)).toFloat();
+            let input = tf.browser.fromPixels(bitmap).toFloat();
+
             console.log(data.imgData.size, input.shape)
             input = tf.expandDims(input, 0);
             if (model){
-                const out = model.predict(input);
-                console.log("OUT", out)
+                const out: tf.Tensor<tf.Rank> = model.predict(input) as tf.Tensor<tf.Rank>;
+                console.log("OUT", out);
+
+                localHiddenCanvasRef.current!.width= out.shape[1]!;
+                localHiddenCanvasRef.current!.height = out.shape[2]!;
+                const reshaped: tf.Tensor<tf.Rank.R3> = out.squeeze([0]);
+                console.log(await reshaped.min().data(),await reshaped.max().data());
+
+                const norm = reshaped.sub(reshaped.min()).div(reshaped.max().sub(reshaped.min()));
+                await tf.browser.toPixels(norm as tf.Tensor<tf.Rank.R3>, localHiddenCanvasRef.current!);
+                localHiddenCanvasRef.current!.toBlob(blob=>{
+                    if (blob){
+                        applyConnectedNodes(id, data, blob);
+                    }
+                })
+
+
 
             }
         }
@@ -47,7 +65,7 @@ const MLNode:React.FC<{data: NodeData, id:string, selected: boolean}> = ({ data,
     return (
     <CustomNode data={data} id={id} inputHandle selected={selected}>
        <button className="text-white"onClick={()=>handleApplyModel()}>YAYA</button> 
-
+       <canvas hidden ref={localHiddenCanvasRef}></canvas>
         <div className='absolute top-4 right-4 w-6 h-6 ml-4 bg-gray-800 rounded-md drag-handle'></div>
     </CustomNode>
   )
